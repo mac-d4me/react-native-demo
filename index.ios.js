98d7d02f369fa52861d1d2c4a7867b5675faa21a
mac-d4me <mac.tao@white-tao.com>
2016-03-31 18:32:30 +0800

    update

---
+++ index.ios.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  NavigatorIOS,
+  StyleSheet,
+} = React;
+
+var SearchScreen = require('./SearchScreen');
+
+var MoviesApp = React.createClass({
+  render: function() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Movies',
+          component: SearchScreen,
+        }}
+      />
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => MoviesApp);
+
+module.exports = MoviesApp;
+
+
